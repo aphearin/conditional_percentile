515e93f25fc0ff0bb27da03b9a5065b727bd340b
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-11 18:04:08 -0500

    Fixed a significant bookeeping bug in correspondence_indices_update. The test_hard_coded_examples module now passes. However, the test_kernels module now throws a segmentation fault whenever it is run

--- test_hard_coded_examples.py
+++ test_hard_coded_examples.py
@@ -8,13 +8,13 @@ from conditional_percentile_kernels import exposed_correspondence_indices_update
 fixed_seed = 43
 
 
-# def test1():
-#     property1 = np.array((5., 4, 3, 2, 1, 0))
-#     property2 = np.array((5., 4, 3, 2, 1, 0))
-#     num_window = 3
-#     endpoint_fill_value = -9
-#     result = conditional_window_ranks(property1, property2, num_window, endpoint_fill_value)
-#     assert np.all(result == [-9, 1, 1, 1, 1, -9])
+def test1():
+    property1 = np.array((5., 4, 3, 2, 1, 0))
+    property2 = np.array((5., 4, 3, 2, 1, 0))
+    num_window = 3
+    endpoint_fill_value = -9
+    result = conditional_window_ranks(property1, property2, num_window, endpoint_fill_value)
+    assert np.all(result == [-9, 1, 1, 1, 1, -9])
 
 
 def test2():
