87a7889d4d0d9179b829bb74c321dd783b7e770b
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-11 19:18:42 -0500

    Added another unit-test of get_initial_windows function

--- test_argument_processing.py
+++ test_argument_processing.py
@@ -11,3 +11,15 @@ def test1():
             sorted_property2, num_window)
     assert np.all(cdf_value_table_init == [3, 4, 5])
     assert np.all(correspondence_indices_init == [2, 1, 0])
+
+
+def test2():
+    sorted_property2 = [0, 1, 2, 5., 4, 3, 2, 1, 0]
+    num_window = 5
+    correspondence_indices_init, cdf_value_table_init = get_initial_windows(
+            sorted_property2, num_window)
+
+    assert np.all(cdf_value_table_init == (0, 1, 2, 4, 5))
+    assert np.all(correspondence_indices_init == (0, 1, 2, 4, 3))
+
+
