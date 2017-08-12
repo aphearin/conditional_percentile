318f524a58e57a2f4e4b9d172cab38aa16d9409b
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-11 19:08:42 -0500

    Refactored some argument processing to aid unit-testing

--- conditional_percentile.py
+++ conditional_percentile.py
@@ -9,21 +9,14 @@ def conditional_window_ranks(property1, property2, num_window=501,
             endpoint_fill_value='auto'):
     """
     """
-    property1 = np.copy(np.atleast_1d(property1))
-    property2 = np.copy(np.atleast_1d(property2))
-
-    #  Bounds checks
-    assert num_window % 2 == 1, "num_window = {0} must be odd".format(num_window)
-    assert property1.shape == property2.shape, "``property1`` and ``property2`` must have same shape"
-    assert len(property1.shape) == 1
-    msg = "Length of ``property1`` = {0} must exceed num_window = {1}"
-    assert property1.shape[0] > num_window, msg.format(property1.shape[0], num_window)
+    property1, property2, num_window, endpoint_fill_value = _process_args(
+            property1, property2, num_window, endpoint_fill_value)
 
     idx_property1_sorted = np.argsort(-property1)
     sorted_property2 = property2[idx_property1_sorted]
 
-    correspondence_indices_init = rank_order_function(sorted_property2[:num_window])
-    cdf_value_table_init = np.sort(sorted_property2[:num_window])
+    correspondence_indices_init, cdf_value_table_init = get_initial_windows(
+            sorted_property2, num_window)
 
     window_ranks = np.array(calculate_percentile_loop(cdf_value_table_init,
             correspondence_indices_init, sorted_property2))
@@ -45,4 +38,33 @@ def rank_order_function(arr):
     """
     idx_sorted = np.argsort(arr)
     return unsorting_indices(idx_sorted)
+
+
+def _process_args(property1, property2, num_window, endpoint_fill_value):
+    """
+    """
+    property1 = np.atleast_1d(property1)
+    property2 = np.atleast_1d(property2)
+
+    #  Bounds checks
+    assert num_window % 2 == 1, "num_window = {0} must be odd".format(num_window)
+    msg1 = "``property1`` and ``property2`` must be 1-d arrays"
+    assert len(property1.shape) == len(property2.shape) == 1, msg1
+
+    msg2 = "``property1`` and ``property2`` must have same shape"
+    assert property1.shape == property2.shape, msg2
+
+    msg3 = "Length of ``property1`` = {0} must exceed num_window = {1}"
+    assert property1.shape[0] > num_window, msg3.format(property1.shape[0], num_window)
+
+    return property1, property2, num_window, endpoint_fill_value
+
+
+def get_initial_windows(sorted_property2, num_window):
+    """
+    """
+    correspondence_indices_init = rank_order_function(sorted_property2[:num_window])
+    cdf_value_table_init = np.sort(sorted_property2[:num_window])
+
+    return correspondence_indices_init, cdf_value_table_init
 
---
+++ test_argument_processing.py
@@ -0,0 +1,13 @@
+"""
+"""
+import numpy as np
+from conditional_percentile import get_initial_windows
+
+
+def test1():
+    sorted_property2 = [5., 4, 3, 2, 1, 0]
+    num_window = 3
+    correspondence_indices_init, cdf_value_table_init = get_initial_windows(
+            sorted_property2, num_window)
+    assert np.all(cdf_value_table_init == [3, 4, 5])
+    assert np.all(correspondence_indices_init == [2, 1, 0])
